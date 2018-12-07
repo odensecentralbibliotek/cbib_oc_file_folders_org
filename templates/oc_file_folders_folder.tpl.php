3b9861303ac235a0fee13e701e7c32d0bbd4444e
memleakz <dev@dev-VirtualBox>
2018-12-07 12:16:27 +0100

    less warnings

--- templates/oc_file_folders_folder.tpl.php
+++ templates/oc_file_folders_folder.tpl.php
@@ -1,5 +1,5 @@
 <?php
-
+$output = "";
 $class = 'oc-is-folder file';
 $folder_name = !empty($row['data']['#file']['description']) ? $row['data']['#file']['description'] : $row['data']['name'];
 $logo = "<img style='float:left;padding-top:3px;margin-right:3px' src='/".drupal_get_path('module', 'cbib_oc_file_folders_org') . "/images/folder.png"."'>";
